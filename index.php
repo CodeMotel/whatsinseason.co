7a59f9e690a8f848aac1bfb960edf0fb89c29a69
Chris Holder <chris@see8ch.com>
2014-03-13 16:44:28 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php include('includes/head.php'); ?>
+
+<!-- Begin Content -->
+<header>
+	<div>Today's<br />Crop</div>
+	<div>MARCH, 15 2014</div>
+	<a href=""><div>Find Markets</div></a>
+</header>
+
+<ul id="crop-list">
+  <li class="apples"><img src="img/apples.svg"/><span>apples</span></li>
+  <li class="apricots"><img src="img/apricots.svg"/><span>apricots</span></li>
+  <li class="asparagus"><img src="img/asparagus.svg"/><span>asparagus</span></li>
+  <li class="beets"><img src="img/beets.svg"/><span>beets</span></li>
+  <li class="bell-peppers"><img src="img/bell-peppers.svg"/><span>bell peppers</span></li>
+  <li class="broccoli"><img src="img/broccoli.svg"/><span>broccoli</span></li>
+  <li class="cabbage"><img src="img/cabbage.svg"/><span>cabbage</span></li>
+  <li class="cantaloupe"><img src="img/cantaloupe.svg"/><span>cantaloupe</span></li>
+  <li class="carrots"><img src="img/carrots.svg"/><span>carrots</span></li>
+  <li class="cauliflower"><img src="img/cauliflower.svg"/><span>cauliflower</span></li>
+  <li class="celery"><img src="img/celery.svg"/><span>celery</span></li>
+  <li class="cherries"><img src="img/cherries.svg"/><span>cherries</span></li>
+  <li class="chile-peppers"><img src="img/chile-peppers.svg"/><span>chile peppers</span></li>
+  <li class="cucumbers"><img src="img/cucumbers.svg"/><span>cucumbers</span></li>
+  <li class="eggplant"><img src="img/eggplant.svg"/><span>eggplant</span></li>
+  <li class="grapes"><img src="img/grapes.svg"/><span>grapes</span></li>
+  <li class="green-beans"><img src="img/green-beans.svg"/><span>green beans</span></li>
+  <li class="herbs"><img src="img/herbs.svg"/><span>herbs</span></li>
+  <li class="honeydew"><img src="img/honeydew.svg"/><span>honeydew</span></li>
+  <li class="lettuce"><img src="img/lettuce.svg"/><span>lettuce</span></li>
+  <li class="onions"><img src="img/onions.svg"/><span>onions</span></li>
+  <li class="peaches"><img src="img/peaches.svg"/><span>peaches</span></li>
+  <li class="pears"><img src="img/pears.svg"/><span>pears</span></li>
+  <li class="pinto-beans"><img src="img/pinto-beans.svg"/><span>pinto beans</span></li>
+  <li class="plums"><img src="img/plums.svg"/><span>plums</span></li>
+  <li class="popcorn"><img src="img/popcorn.svg"/><span>popcorn</span></li>
+  <li class="potatos"><img src="img/potatos.svg"/><span>potatos</span></li>
+  <li class="pumpkins"><img src="img/pumpkins.svg"/><span>pumpkins</span></li>
+  <li class="raspberries"><img src="img/raspberries.svg"/><span>raspberries</span></li>
+  <li class="rhubarb"><img src="img/rhubarb.svg"/><span>rhubarb</span></li>
+  <li class="squash"><img src="img/squash.svg"/><span>squash</span></li>
+  <li class="tomatoes"><img src="img/tomatoes.svg"/><span>tomatoes</span></li>
+  <li class="watermelon"><img src="img/watermelon.svg"/><span>watermelon</span></li>
+</ul>
+
+<footer>
+	<div>Find a Market<br /><a href="">Go ></a></div>
+	<div>About</div>
+	<div>Add Your City</div>
+</footer>
+
+<?php include('includes/footer.php'); ?>
