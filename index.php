56d5844e00028d763b94c1e7dbbf02eea9f524a6
Chris Holder <chris@see8ch.com>
2014-03-14 20:52:34 -0600

    Basic structure/missing things/svg to php
    — Basic content structure
    — Converted svg icons to php
    — Added missing items in php and scss

--- index.php
+++ index.php
@@ -2,51 +2,53 @@
 
 <!-- Begin Content -->
 <header>
-	<div>Today's<br />Crop</div>
-	<div>MARCH, 15 2014</div>
-	<a href=""><div>Find Markets</div></a>
+  <div>Today's<br />Crop</div>
+  <div>March, 15 2014</div>
+  <a href="http://www.coloradofreshmarkets.com/markets.html"><div>Find Markets</div></a>
 </header>
 
 <ul id="crop-list">
-  <li class="apples"><img src="img/apples.svg"/><span>apples</span></li>
-  <li class="apricots"><img src="img/apricots.svg"/><span>apricots</span></li>
-  <li class="asparagus"><img src="img/asparagus.svg"/><span>asparagus</span></li>
-  <li class="beets"><img src="img/beets.svg"/><span>beets</span></li>
-  <li class="bell-peppers"><img src="img/bell-peppers.svg"/><span>bell peppers</span></li>
-  <li class="broccoli"><img src="img/broccoli.svg"/><span>broccoli</span></li>
-  <li class="cabbage"><img src="img/cabbage.svg"/><span>cabbage</span></li>
-  <li class="cantaloupe"><img src="img/cantaloupe.svg"/><span>cantaloupe</span></li>
-  <li class="carrots"><img src="img/carrots.svg"/><span>carrots</span></li>
-  <li class="cauliflower"><img src="img/cauliflower.svg"/><span>cauliflower</span></li>
-  <li class="celery"><img src="img/celery.svg"/><span>celery</span></li>
-  <li class="cherries"><img src="img/cherries.svg"/><span>cherries</span></li>
-  <li class="chile-peppers"><img src="img/chile-peppers.svg"/><span>chile peppers</span></li>
-  <li class="cucumbers"><img src="img/cucumbers.svg"/><span>cucumbers</span></li>
-  <li class="eggplant"><img src="img/eggplant.svg"/><span>eggplant</span></li>
-  <li class="grapes"><img src="img/grapes.svg"/><span>grapes</span></li>
-  <li class="green-beans"><img src="img/green-beans.svg"/><span>green beans</span></li>
-  <li class="herbs"><img src="img/herbs.svg"/><span>herbs</span></li>
-  <li class="honeydew"><img src="img/honeydew.svg"/><span>honeydew</span></li>
-  <li class="lettuce"><img src="img/lettuce.svg"/><span>lettuce</span></li>
-  <li class="onions"><img src="img/onions.svg"/><span>onions</span></li>
-  <li class="peaches"><img src="img/peaches.svg"/><span>peaches</span></li>
-  <li class="pears"><img src="img/pears.svg"/><span>pears</span></li>
-  <li class="pinto-beans"><img src="img/pinto-beans.svg"/><span>pinto beans</span></li>
-  <li class="plums"><img src="img/plums.svg"/><span>plums</span></li>
-  <li class="popcorn"><img src="img/popcorn.svg"/><span>popcorn</span></li>
-  <li class="potatos"><img src="img/potatos.svg"/><span>potatos</span></li>
-  <li class="pumpkins"><img src="img/pumpkins.svg"/><span>pumpkins</span></li>
-  <li class="raspberries"><img src="img/raspberries.svg"/><span>raspberries</span></li>
-  <li class="rhubarb"><img src="img/rhubarb.svg"/><span>rhubarb</span></li>
-  <li class="squash"><img src="img/squash.svg"/><span>squash</span></li>
-  <li class="tomatoes"><img src="img/tomatoes.svg"/><span>tomatoes</span></li>
-  <li class="watermelon"><img src="img/watermelon.svg"/><span>watermelon</span></li>
+  <li class="apples"><?php include('includes/food/apples.php'); ?><span>apples</span></li>
+  <li class="apricots"><?php include('includes/food/apricots.php'); ?><span>apricots</span></li>
+  <li class="asparagus"><?php include('includes/food/asparagus.php'); ?><span>asparagus</span></li>
+  <li class="beets"><?php include('includes/food/beets.php'); ?><span>beets</span></li>
+  <li class="bell-peppers"><?php include('includes/food/bell-peppers.php'); ?><span>bell peppers</span></li>
+  <li class="broccoli"><?php include('includes/food/broccoli.php'); ?><span>broccoli</span></li>
+  <li class="cabbage"><?php include('includes/food/cabbage.php'); ?><span>cabbage</span></li>
+  <li class="cantaloupe"><?php include('includes/food/cantaloupe.php'); ?><span>cantaloupe</span></li>
+  <li class="carrots"><?php include('includes/food/carrots.php'); ?><span>carrots</span></li>
+  <li class="cauliflower"><?php include('includes/food/cauliflower.php'); ?><span>cauliflower</span></li>
+  <li class="celery"><?php include('includes/food/celery.php'); ?><span>celery</span></li>
+  <li class="cherries"><?php include('includes/food/cherries.php'); ?><span>cherries</span></li>
+  <li class="chile-peppers"><?php include('includes/food/chile-peppers.php'); ?><span>chile peppers</span></li>
+  <li class="cucumbers"><?php include('includes/food/cucumbers.php'); ?><span>cucumbers</span></li>
+  <li class="eggplant"><?php include('includes/food/eggplant.php'); ?><span>eggplant</span></li>
+  <li class="grapes"><?php include('includes/food/grapes.php'); ?><span>grapes</span></li>
+  <li class="green-beans"><?php include('includes/food/green-beans.php'); ?><span>green beans</span></li>
+  <li class="herbs"><?php include('includes/food/herbs.php'); ?><span>herbs</span></li>
+  <li class="honeydew"><?php include('includes/food/honeydew.php'); ?><span>honeydew</span></li>
+  <li class="lettuce"><?php include('includes/food/lettuce.php'); ?><span>lettuce</span></li>
+  <li class="onions"><?php include('includes/food/onions.php'); ?><span>onions</span></li>
+  <li class="peaches"><?php include('includes/food/peaches.php'); ?><span>peaches</span></li>
+  <li class="pears"><?php include('includes/food/pears.php'); ?><span>pears</span></li>
+  <li class="pinto-beans"><?php include('includes/food/pinto-beans.php'); ?><span>pinto beans</span></li>
+  <li class="plums"><?php include('includes/food/plums.php'); ?><span>plums</span></li>
+  <li class="popcorn"><?php include('includes/food/popcorn.php'); ?><span>popcorn</span></li>
+  <li class="potatos"><?php include('includes/food/potatos.php'); ?><span>potatos</span></li>
+  <li class="pumpkins"><?php include('includes/food/pumpkins.php'); ?><span>pumpkins</span></li>
+  <li class="raspberries"><?php include('includes/food/raspberries.php'); ?><span>raspberries</span></li>
+  <li class="rhubarb"><?php include('includes/food/rhubarb.php'); ?><span>rhubarb</span></li>
+  <li class="squash"><?php include('includes/food/squash.php'); ?><span>squash</span></li>
+  <li class="strawberries"><?php include('includes/food/strawberries.php'); ?><span>strawberries</span></li>
+  <li class="sweet-corn"><?php include('includes/food/sweet-corn.php'); ?><span>sweet-corn</span></li>
+  <li class="tomatoes"><?php include('includes/food/tomatoes.php'); ?><span>tomatoes</span></li>
+  <li class="watermelon"><?php include('includes/food/watermelon.php'); ?><span>watermelon</span></li>
 </ul>
 
 <footer>
-	<div>Find a Market<br /><a href="">Go ></a></div>
-	<div>About</div>
-	<div>Add Your City</div>
+  <div>Find a Market<br /><a href="http://www.coloradofreshmarkets.com/markets.html">Go ></a></div>
+  <div>About</div>
+  <div>Add Your City</div>
 </footer>
 
 <?php include('includes/footer.php'); ?>
