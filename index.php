899fab0d26084034830c4b677071b1ad634055aa
Chris Holder <chris@see8ch.com>
2014-03-16 03:02:34 -0600

    Added date ranges for food items

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <!-- Date -->
 <div id="current-date">
-   <div id="date-header">Farm Fresh Today</div>
+   <div id="date-header">Fresh in Colorado</div>
   <script type="text/javascript">
       function makeArray() {
       for (i = 0; i<makeArray.arguments.length; i++)
@@ -31,48 +31,48 @@
 <div id="crop-wrap" class="wrapper">
    <!--<div id="crop-header">Today's Crops</div>-->
    <ul id="crop-list">
-     <li class="apples"><?php include('includes/food/apples.php'); ?><span>apples</span></li>
-     <li class="apricots"><?php include('includes/food/apricots.php'); ?><span>apricots</span></li>
-     <li class="asparagus"><?php include('includes/food/asparagus.php'); ?><span>asparagus</span></li>
-     <li class="beets"><?php include('includes/food/beets.php'); ?><span>beets</span></li>
-     <li class="bell-peppers"><?php include('includes/food/bell-peppers.php'); ?><span>bell peppers</span></li>
-     <li class="broccoli"><?php include('includes/food/broccoli.php'); ?><span>broccoli</span></li>
-     <li class="cabbage"><?php include('includes/food/cabbage.php'); ?><span>cabbage</span></li>
-     <li class="cantaloupe"><?php include('includes/food/cantaloupe.php'); ?><span>cantaloupe</span></li>
-     <li class="carrots"><?php include('includes/food/carrots.php'); ?><span>carrots</span></li>
-     <li class="cauliflower"><?php include('includes/food/cauliflower.php'); ?><span>cauliflower</span></li>
-     <li class="celery"><?php include('includes/food/celery.php'); ?><span>celery</span></li>
-     <li class="cherries"><?php include('includes/food/cherries.php'); ?><span>cherries</span></li>
-     <li class="chile-peppers"><?php include('includes/food/chile-peppers.php'); ?><span>chile peppers</span></li>
-     <li class="cucumbers"><?php include('includes/food/cucumbers.php'); ?><span>cucumbers</span></li>
-     <li class="eggplant"><?php include('includes/food/eggplant.php'); ?><span>eggplant</span></li>
-     <li class="grapes"><?php include('includes/food/grapes.php'); ?><span>grapes</span></li>
-     <li class="green-beans"><?php include('includes/food/green-beans.php'); ?><span>green beans</span></li>
-     <li class="herbs"><?php include('includes/food/herbs.php'); ?><span>herbs</span></li>
-     <li class="honeydew"><?php include('includes/food/honeydew.php'); ?><span>honeydew</span></li>
-     <li class="lettuce"><?php include('includes/food/lettuce.php'); ?><span>lettuce</span></li>
-     <li class="onions"><?php include('includes/food/onions.php'); ?><span>onions</span></li>
-     <li class="peaches"><?php include('includes/food/peaches.php'); ?><span>peaches</span></li>
-     <li class="pears"><?php include('includes/food/pears.php'); ?><span>pears</span></li>
-     <li class="pinto-beans"><?php include('includes/food/pinto-beans.php'); ?><span>pinto beans</span></li>
-     <li class="plums"><?php include('includes/food/plums.php'); ?><span>plums</span></li>
-     <li class="popcorn"><?php include('includes/food/popcorn.php'); ?><span>popcorn</span></li>
-     <li class="potatoes"><?php include('includes/food/potatoes.php'); ?><span>potatoes</span></li>
-     <li class="pumpkins"><?php include('includes/food/pumpkins.php'); ?><span>pumpkins</span></li>
-     <li class="raspberries"><?php include('includes/food/raspberries.php'); ?><span>raspberries</span></li>
-     <li class="rhubarb"><?php include('includes/food/rhubarb.php'); ?><span>rhubarb</span></li>
-     <li class="squash"><?php include('includes/food/squash.php'); ?><span>squash</span></li>
-     <li class="strawberries"><?php include('includes/food/strawberries.php'); ?><span>strawberries</span></li>
-     <li class="sweet-corn"><?php include('includes/food/sweet-corn.php'); ?><span>sweet-corn</span></li>
-     <li class="tomatoes"><?php include('includes/food/tomatoes.php'); ?><span>tomatoes</span></li>
-     <li class="watermelon"><?php include('includes/food/watermelon.php'); ?><span>watermelon</span></li>
+     <li id="apples" class="apples"><?php include('includes/food/apples.php'); ?><span>apples</span></li>
+     <li id="apricots" class="apricots"><?php include('includes/food/apricots.php'); ?><span>apricots</span></li>
+     <li id="asparagus" class="asparagus"><?php include('includes/food/asparagus.php'); ?><span>asparagus</span></li>
+     <li id="beets" class="beets"><?php include('includes/food/beets.php'); ?><span>beets</span></li>
+     <li id="bell-peppers" class="bell-peppers"><?php include('includes/food/bell-peppers.php'); ?><span>bell peppers</span></li>
+     <li id="broccoli" class="broccoli"><?php include('includes/food/broccoli.php'); ?><span>broccoli</span></li>
+     <li id="cabbage" class="cabbage"><?php include('includes/food/cabbage.php'); ?><span>cabbage</span></li>
+     <li id="cantaloupe" class="cantaloupe"><?php include('includes/food/cantaloupe.php'); ?><span>cantaloupe</span></li>
+     <li id="carrots" class="carrots"><?php include('includes/food/carrots.php'); ?><span>carrots</span></li>
+     <li id="cauliflower" class="cauliflower"><?php include('includes/food/cauliflower.php'); ?><span>cauliflower</span></li>
+     <li id="celery" class="celery"><?php include('includes/food/celery.php'); ?><span>celery</span></li>
+     <li id="cherries" class="cherries"><?php include('includes/food/cherries.php'); ?><span>cherries</span></li>
+     <li id="chile-peppers" class="chile-peppers"><?php include('includes/food/chile-peppers.php'); ?><span>chile peppers</span></li>
+     <li id="cucumbers" class="cucumbers"><?php include('includes/food/cucumbers.php'); ?><span>cucumbers</span></li>
+     <li id="eggplant" class="eggplant"><?php include('includes/food/eggplant.php'); ?><span>eggplant</span></li>
+     <li id="grapes" class="grapes"><?php include('includes/food/grapes.php'); ?><span>grapes</span></li>
+     <li id="green-beans" class="green-beans"><?php include('includes/food/green-beans.php'); ?><span>green beans</span></li>
+     <li id="herbs" class="herbs"><?php include('includes/food/herbs.php'); ?><span>herbs</span></li>
+     <li id="honeydew" class="honeydew"><?php include('includes/food/honeydew.php'); ?><span>honeydew</span></li>
+     <li id="lettuce" class="lettuce"><?php include('includes/food/lettuce.php'); ?><span>lettuce</span></li>
+     <li id="onions" class="onions"><?php include('includes/food/onions.php'); ?><span>onions</span></li>
+     <li id="peaches" class="peaches"><?php include('includes/food/peaches.php'); ?><span>peaches</span></li>
+     <li id="pears" class="pears"><?php include('includes/food/pears.php'); ?><span>pears</span></li>
+     <li id="pinto-beans" class="pinto-beans"><?php include('includes/food/pinto-beans.php'); ?><span>pinto beans</span></li>
+     <li id="plums" class="plums"><?php include('includes/food/plums.php'); ?><span>plums</span></li>
+     <li id="popcorn" class="popcorn"><?php include('includes/food/popcorn.php'); ?><span>popcorn</span></li>
+     <li id="potatoes" class="potatoes"><?php include('includes/food/potatoes.php'); ?><span>potatoes</span></li>
+     <li id="pumpkins" class="pumpkins"><?php include('includes/food/pumpkins.php'); ?><span>pumpkins</span></li>
+     <li id="raspberries" class="raspberries"><?php include('includes/food/raspberries.php'); ?><span>raspberries</span></li>
+     <li id="rhubarb" class="rhubarb"><?php include('includes/food/rhubarb.php'); ?><span>rhubarb</span></li>
+     <li id="squash" class="squash"><?php include('includes/food/squash.php'); ?><span>squash</span></li>
+     <li id="strawberries" class="strawberries"><?php include('includes/food/strawberries.php'); ?><span>strawberries</span></li>
+     <li id="sweet-corn" class="sweet-corn"><?php include('includes/food/sweet-corn.php'); ?><span>sweet-corn</span></li>
+     <li id="tomatoes" class="tomatoes"><?php include('includes/food/tomatoes.php'); ?><span>tomatoes</span></li>
+     <li id="watermelon" class="watermelon"><?php include('includes/food/watermelon.php'); ?><span>watermelon</span></li>
    </ul>
 </div>
 
 <footer>
-  <div>Find a Market<br /><a href="http://www.coloradofreshmarkets.com/markets.html">View Map ></a></div>
-  <div>About</div>
-  <div>Add Your City</div>
+  <div id="footer-market">Find a Market<br /><a href="http://www.coloradofreshmarkets.com/markets.html">View Map ></a></div>
+  <div id="about-module" class="footer-toggles">About</div>
+  <div id="city-module" class="footer-toggles">Add Your City</div>
 </footer>
 
 <div id="bkgd-img"></div>
