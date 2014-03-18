141d6adf353be5eee422ba80263de927dd7ee1c6
Chris Holder <chris@see8ch.com>
2014-03-18 15:48:05 -0600

    responsive

--- includes/footer.php
+++ includes/footer.php
@@ -57,6 +57,18 @@
         <!-- Modernizr -->
         <script src="js/modernizr.js"></script>
 
+        <!-- Browser Update Mess -->
+        <script type="text/javascript">
+          var $buoop = {};
+          $buoop.ol = window.onload;
+          window.onload=function(){
+           try {if ($buoop.ol) $buoop.ol();}catch (e) {}
+           var e = document.createElement("script");
+           e.setAttribute("type", "text/javascript");
+           e.setAttribute("src", "//browser-update.org/update.js");
+           document.body.appendChild(e);
+          }
+        </script>
 
         <!-- Google Analytics -->
         <script type="text/javascript">
