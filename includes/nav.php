0cbb53f77c4216dada6c69d1a379a8b0073bc026
Chris Holder <chris@see8ch.com>
2014-03-18 14:57:22 -0600

    remove share temp

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,5 @@
 <!--******** Header Region ********-->
-<div class="bookmark-wrapper">
-	<div id="bookmark">
-		<p><span class="genericon genericon-pinned"></span>bookmark us or save to your home screen </p>
-		<p id="share">share <a href="http://twitter.com/share?text=What&#39;s%20in%20Season&#44;%20Colorado&#63;%20http%3A%2F%2Fwhatsinseason.co%20%40whatsinseasonco"><span class="genericon genericon-twitter"></span></a> <span class="genericon genericon-facebook"></span> <span class="genericon genericon-pinterest-alt"></span></p>
-	</div>
-</div>
+
 <header>
    <div class="header-wrapper">
       <div id="site-logo">what's in season?</div>
