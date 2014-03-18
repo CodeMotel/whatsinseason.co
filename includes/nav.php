1b5199dcacd334fca076302504046aa2f2b0700d
Chris Holder <chris@see8ch.com>
2014-03-18 15:26:36 -0600

    share widget

--- includes/nav.php
+++ includes/nav.php
@@ -8,3 +8,9 @@
    </div>
 </header>
 
+<div class="bookmark-wrapper">
+	<div id="bookmark">
+		<p><span class="genericon genericon-pinned"></span>bookmark us or save to your home screen </p>
+		<p id="share">share <a target="_blank" href="https://twitter.com/home?status=What's%20in%20Season,%20CO?%20Find%20out%20what's%20fresh%20today%20in%20Denver%20http://whatsinseason.co%20@whatsinseasonco"><span class="genericon genericon-twitter"></span></a> <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://whatsinseason.co"><span class="genericon genericon-facebook"></span></a> <a target="_blank" href="https://plus.google.com/share?url=whatsinseason.co"><span class="genericon genericon-googleplus"></span></a></p>
+	</div>
+</div>
