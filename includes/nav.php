de2eaa26e3176fd32919f91fc8ead395646f2832
Chris Holder <chris@see8ch.com>
2014-03-20 10:28:35 -0600

    header responsive tweaks

--- includes/nav.php
+++ includes/nav.php
@@ -10,7 +10,7 @@
 <div class="bookmark-wrapper">
 	<div id="bookmark">
 		<p class="bookmark-msg"><span class="genericon genericon-pinned"></span>bookmark us</p>
-		<p class="mobile-msg"><span class="genericon genericon-phone"></span> save us to your home screen</p>
+		<p class="mobile-msg"><span class="genericon genericon-phone"></span> bookmark us<!--save us to your home screen--></p>
 		<p id="share">share <a target="_blank" href="https://twitter.com/home?status=What's%20in%20Season,%20CO?%20Find%20out%20what's%20fresh%20today%20in%20Denver%20http://whatsinseason.co%20@whatsinseasonco"><span class="genericon genericon-twitter"></span></a> <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://whatsinseason.co"><span class="genericon genericon-facebook"></span></a> <a target="_blank" href="https://plus.google.com/share?url=whatsinseason.co"><span class="genericon genericon-googleplus"></span></a></p>
 	</div>
 </div>
