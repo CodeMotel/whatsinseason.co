678e5fac8692f27da16e3fe9ac323efa47ef902a
Chris Holder <chris@see8ch.com>
2014-03-22 21:56:29 -0600

    Possible iOS input bug fix

--- js/main.js
+++ js/main.js
@@ -101,3 +101,19 @@ $("#map-toggle-2").click(function(){
 $("#map-close").click(function(){
   $("#map").toggleClass("active");
 });
+
+
+/* we need this only on touch devices */
+if (Modernizr.touch) {
+    /* cache dom references */
+    var $body = jQuery('body');
+
+    /* bind events */
+    $(document)
+    .on('focus', 'input', function(e) {
+        $body.addClass('fixfixed');
+    })
+    .on('blur', 'input', function(e) {
+        $body.removeClass('fixfixed');
+    });
+}
